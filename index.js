436bbf6599b2748d2ce98f7400e33d3545fb4d28
Ederson Torresini <ederson@boidacarapreta.cc>
2020-11-26 03:46:16 +0000

    Iniciar tutorial do Phaser 3

---
+++ index.js
@@ -0,0 +1,43 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var game = new Phaser.Game(config);
+var platforms;
+
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+
+function create() {
+  this.add.image(400, 300, "sky");
+
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+}
+
+function update() {}
