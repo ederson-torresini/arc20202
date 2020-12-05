57ca903ebde5f15a9ec2b864e590a8d901a7f72e
Ederson Torresini <ederson@boidacarapreta.cc>
2020-12-05 03:53:02 +0000

    Testar tarefas #9, #10 e #11

--- index.js
+++ index.js
@@ -1,43 +1,108 @@
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
+  height: 608,
+  parent: "game-container",
   scene: {
     preload: preload,
     create: create,
-    update: update,
+    update: update
   },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
 };
 
-var game = new Phaser.Game(config);
-var platforms;
+const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("ground", "assets/platform.png");
-  this.load.image("star", "assets/star.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dude.png", {
-    frameWidth: 32,
-    frameHeight: 48,
+  this.load.image("terreno", "assets/terreno.png");
+  this.load.image("ARCas", "assets/ARCas.png");
+  this.load.tilemapTiledJSON("map", "assets/cena-1.json");
+  this.load.spritesheet("player", "assets/player.png", {
+    frameWidth: 16,
+    frameHeight: 16
   });
 }
 
 function create() {
-  this.add.image(400, 300, "sky");
+  const map = this.make.tilemap({ key: "map" });
 
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 568, "ground").setScale(2).refreshBody();
-  platforms.create(600, 400, "ground");
-  platforms.create(50, 250, "ground");
-  platforms.create(750, 220, "ground");
+  const tileset0 = map.addTilesetImage("terreno", "terreno");
+  const tileset1 = map.addTilesetImage("ARCas", "ARCas");
+
+  const terreno = map.createStaticLayer("terreno", tileset0, 0, 0);
+  const ARCas = map.createStaticLayer("ARCas", tileset1, 0, 0);
+
+  player = this.physics.add.sprite(400, 304, "player");
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 0,
+      end: 6
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 15,
+      end: 21
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "stopped",
+    frames: this.anims.generateFrameNumbers("player", {
+      start: 11,
+      end: 14
+    }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  ARCas.setCollisionBetween(0, 9999);
+  // ARCas.setCollisionByProperty({ "collides": true });
+  // const debugGraphics = this.add.graphics().setAlpha(0.75);
+  // ARCas.renderDebug(debugGraphics, {
+  //   tileColor: null,
+  //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+  //   faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+  // });
+
+  this.physics.add.collider(player, ARCas);
 }
 
-function update() {}
+function update(time, delta) {
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-100);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(100);
+    player.anims.play("right", true);
+  } else {
+    player.body.setVelocity(0);
+    player.anims.play("stopped", true);
+  }
+
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-100);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(100);
+  } else { 
+    player.body.setVelocityY(0);
+  }
+
+  //player.body.velocity.normalize().scale(speed);
+}
