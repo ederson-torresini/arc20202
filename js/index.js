830e46156849b384cff902149c820b18af717453
Ederson Torresini <ederson@boidacarapreta.cc>
2020-12-09 19:51:01 -0300

    Terminar detecção de colisão #12

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ const config = {
   scene: {
     preload: preload,
     create: create,
-    update: update
+    update: update,
   },
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 0 }
-    }
-  }
+      gravity: { y: 0 },
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
@@ -24,64 +24,57 @@ function preload() {
   this.load.tilemapTiledJSON("map", "assets/cena-1.json");
   this.load.spritesheet("player", "assets/player.png", {
     frameWidth: 16,
-    frameHeight: 16
+    frameHeight: 16,
   });
 }
 
 function create() {
+  // Tilemap
   const map = this.make.tilemap({ key: "map" });
-
   const tileset0 = map.addTilesetImage("terreno", "terreno");
   const tileset1 = map.addTilesetImage("ARCas", "ARCas");
-
   const terreno = map.createStaticLayer("terreno", tileset0, 0, 0);
   const ARCas = map.createStaticLayer("ARCas", tileset1, 0, 0);
 
+  // Player
   player = this.physics.add.sprite(400, 304, "player");
+
+  // Collision detection
+  ARCas.setCollisionByProperty({ "collides": true });
   player.setCollideWorldBounds(true);
+  this.physics.add.collider(player, ARCas);
 
   this.anims.create({
     key: "left",
     frames: this.anims.generateFrameNumbers("player", {
       start: 0,
-      end: 6
+      end: 6,
     }),
     frameRate: 10,
-    repeat: -1
+    repeat: -1,
   });
 
   this.anims.create({
     key: "right",
     frames: this.anims.generateFrameNumbers("player", {
       start: 15,
-      end: 21
+      end: 21,
     }),
     frameRate: 10,
-    repeat: -1
+    repeat: -1,
   });
 
   this.anims.create({
     key: "stopped",
     frames: this.anims.generateFrameNumbers("player", {
       start: 11,
-      end: 14
+      end: 14,
     }),
     frameRate: 5,
-    repeat: -1
+    repeat: -1,
   });
 
   cursors = this.input.keyboard.createCursorKeys();
-
-  ARCas.setCollisionBetween(0, 9999);
-  // ARCas.setCollisionByProperty({ "collides": true });
-  // const debugGraphics = this.add.graphics().setAlpha(0.75);
-  // ARCas.renderDebug(debugGraphics, {
-  //   tileColor: null,
-  //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
-  //   faceColor: new Phaser.Display.Color(40, 39, 37, 255)
-  // });
-
-  this.physics.add.collider(player, ARCas);
 }
 
 function update(time, delta) {
@@ -100,9 +93,7 @@ function update(time, delta) {
     player.body.setVelocityY(-100);
   } else if (cursors.down.isDown) {
     player.body.setVelocityY(100);
-  } else { 
+  } else {
     player.body.setVelocityY(0);
   }
-
-  //player.body.velocity.normalize().scale(speed);
 }
